a0ff3c0634a83ada52b04091e8ed4f0135468c45
miketti <acepost@protonmail.ch>
2015-10-23 12:54:51 +0300

    init

---
+++ Controllers/PagesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication3.Controllers
+{
+    public class PagesController : Controller
+    {
+        //
+        // GET: /Pages/
+
+        public ActionResult Index(string param1)
+        {
+            string ViewFileName = "~/Views/Pages/" + param1;
+
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView(ViewFileName);
+            }
+            else
+            {
+                return View(ViewFileName);
+            }
+        }
+
+    }
+}
---
+++ Models/Post.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication3.Models
+{
+    public class Post
+    {
+        public string Id;
+        public string Title;
+        public string Description;
+        public string Content;
+        public DateTime Date;
+        public string DateString
+        {
+            get { return Date.ToString("dd.MM.yyyy"); }
+        }
+
+        public Post()
+        {
+            Id = "";
+            Title = "";
+            Description = "";
+            Content = "";
+            Date = DateTime.MinValue;
+        }
+    }
+}
